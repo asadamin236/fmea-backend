b9d51de6759fa84b18c6f908c0b41fdb15aea568
Asad Amin Data Analyst | PowerBI Developer <asadamin236@gmail.com>
2025-07-11 18:39:37 +0500

    updated backend

--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,13 @@ import equipmentClassRoutes from "./routes/equipmentClass.routes";
 import equipmentTypeRoutes from "./routes/equipmentType.routes";
 import teamRoutes from "./routes/team.routes";
 import userRoutes from "./routes/user.routes";
-import componentRoutes from './routes/component.routes';
+import componentRoutes from "./routes/component.routes";
 
 app.use("/api/auth", authRoutes);
 app.use("/api/equipment-class", equipmentClassRoutes);
 app.use("/api/equipment-types", equipmentTypeRoutes);
 app.use("/api/teams", teamRoutes);
 app.use("/api/users", userRoutes);
-app.use('/api/components', componentRoutes);
+app.use("/api/components", componentRoutes);
 
 export default app; // ✅ Export app for serverless use
