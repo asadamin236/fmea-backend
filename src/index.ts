3fe1ccd9a5e7cb4c2c15dbba90bc5947b633c67b
Asad Amin Data Analyst | PowerBI Developer <asadamin236@gmail.com>
2025-07-11 20:14:27 +0500

    Code of index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,22 @@ const app = express();
 connectDB();
 
 const allowedOrigins = [
-  'http://localhost:5000',
-  'https://fmea-frontend.vercel.app'
+  "http://localhost:5000",
+  "https://fmea-frontend.vercel.app",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
